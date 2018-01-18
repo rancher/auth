d1d32bacf99a0344fa45bb51997d2a83870c5104
Prachi Damle <prachi@rancher.com>
2018-01-17 19:32:01 -0800

    Adding norman server for tokens handler
    Refetch token using schema.store for pretty norman output
    
    Delete Token should delete via store to fetch the data back with removedTS set
    
    Set description to tokens
    
    cleanup main

--- main.go
+++ main.go
@@ -1,97 +1,39 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
-	"github.com/rancher/auth/principals"
-	"github.com/rancher/auth/tokens"
-	log "github.com/sirupsen/logrus"
-	"github.com/urfave/cli"
-
-	"github.com/rancher/types/config"
-
 	"k8s.io/client-go/tools/clientcmd"
+
+	"github.com/rancher/auth/server"
+	"github.com/rancher/types/config"
 )
 
-var VERSION = "v0.0.0-dev"
-
 func main() {
-	app := cli.NewApp()
-	app.Name = "auth"
-	app.Version = VERSION
-	app.Usage = "You need help!"
-	app.Action = run
-
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "cluster-config",
-			Usage: "Kube config for accessing cluster",
-		},
-		cli.StringFlag{
-			Name:  "cluster-name",
-			Usage: "name of the cluster",
-		},
-		cli.StringFlag{
-			Name:  "httpHost",
-			Usage: "host:port to listen on",
-		},
+	if err := run(); err != nil {
+		panic(err)
 	}
-
-	app.Run(os.Args)
 }
 
-func run(c *cli.Context) {
-
-	mgmtCtx, err := setupClient(c.String("cluster-config"), c.String("cluster-config"), "")
+func run() error {
+	kubeConfig, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
 	if err != nil {
-		log.Fatalf("Failed to create ManagementContext: %v", err)
+		return err
 	}
 
-	tokenHandler, err := tokens.NewTokenAPIHandler(nil, mgmtCtx)
+	mgmtCtx, err := config.NewManagementContext(*kubeConfig)
 	if err != nil {
-		log.Fatalf("Failed to get tokenAndPrincipal handler: %v", err)
+		return err
 	}
 
-	principalHandler, err := principals.NewPrincipalAPIHandler(nil, mgmtCtx)
+	handler, err := server.NewTokenAPIHandler(context.Background(), mgmtCtx)
 	if err != nil {
-		log.Fatalf("Failed to get NewPrincipalAPIHandler handler: %v", err)
+		return err
 	}
 
-	if c.GlobalBool("debug") {
-		log.SetLevel(log.DebugLevel)
-	}
-
-	textFormatter := &log.TextFormatter{
-		FullTimestamp: true,
-	}
-	log.SetFormatter(textFormatter)
-
-	log.Info("Starting Rancher Auth proxy")
-
-	httpHost := c.GlobalString("httpHost")
-
-	router := mux.NewRouter()
-	router.Handle("/v3/tokens", tokenHandler).Methods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD")
-	router.Handle("/v3/principals", principalHandler).Methods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD")
-	router.Handle("/v3/authconfigs", tokenHandler).Methods("GET")
-
-	log.Infof("Starting http server listening on %v.", httpHost)
-	log.Fatal(http.ListenAndServe(httpHost, router))
-
-}
-
-func setupClient(clusterManagerCfg string, clusterCfg string, clusterName string) (*config.ManagementContext, error) {
-	clusterManagementKubeConfig, err := clientcmd.BuildConfigFromFlags("", clusterManagerCfg)
-	if err != nil {
-		return nil, err
-	}
-	workload, err := config.NewManagementContext(*clusterManagementKubeConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	return workload, nil
-
+	fmt.Println("Listening on 0.0.0.0:1234")
+	return http.ListenAndServe("0.0.0.0:1234", handler)
 }
